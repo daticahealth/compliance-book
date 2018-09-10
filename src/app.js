b89738ec3dbcf0b8ce4569a72979ae6ad519d093
R. Allan White <allan@catalyze.io>
2018-09-10 15:34:36 -0700

    Added Lozad. Basic unstyled content.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 import * as offline from 'offline-plugin/runtime';
 import './app.styl';
 import './components/intro/intro.js';
-
+import lozad from 'lozad';
 
 offline.install({
   onUpdateReady: function() {
     offline.applyUpdate();
   }
 });
+
+// window.$ = $;
+// const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+const observer = lozad('.lozad', {
+    // rootMargin: '100px 0px', // like css margin
+    // threshold: 0.1 // ratio of element convergence
+    loaded: function(el) {
+        // Custom implementation on a loaded element
+        el.classList.add('loaded');
+    }
+});
+observer.observe();
